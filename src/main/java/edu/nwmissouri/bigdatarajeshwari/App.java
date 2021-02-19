311010430805395a137b7983608f57d7fa5c892a
Rajeshwari-Rudra <rajeshwarihyd.02@gmail.com>
2021-02-19 14:41:51 -0600

    maven initial commit

---
+++ src/main/java/edu/nwmissouri/bigdatarajeshwari/App.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.bigdatarajeshwari;
+
+/**
+ * This is my demo maven-java-app. 
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
